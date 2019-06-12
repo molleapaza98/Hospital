90098063e4661b43e13c2a7c743c8d832dfe6e69
Erick Alexis Molleapaza Palomino <molleapaza98@gmail.com>
2019-06-12 02:30:47 -0500

    Crear Clase Specility

---
+++ hospitalcore/src/main/java/com/mitocode/mode/entity/Specialty.java
@@ -0,0 +1,16 @@
+package com.mitocode.mode.entity;
+
+public class Specialty {
+	
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+
+}
